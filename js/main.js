fb4ff1aa92137acde02a699f8a82fee1d0fb1487
BruceZhou <peichunzhou1006@gmail.com>
2018-05-22 11:40:23 +1000

    init repo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// base https://wind-bow.gomix.me/twitch-api
+// /users/:user, /channels/:channel, and /streams/:stream
+// var timer = null
+// if (timer) {
+//   clearTimeout(timer)
+// }
+// timer = setTimeout(function () {
+//   $.ajax({
+//     url: 'https://wind-bow.gomix.me/twitch-api/users/ChiveyX',
+//     dataType: 'jsonp',
+//     success: function (data) {
+      
+//       console.log(data)
+//     }
+//   })
+// }, 500)
